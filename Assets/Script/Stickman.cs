4cfc79711ba7d8a57a65e4544154e70ef701e7db
ofgonen_office <ofgonen@gmail.com>
2022-10-20 23:03:25 +0300

    Stickman: AttachToVehicle

---
+++ Assets/Script/Stickman.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+using Sirenix.OdinInspector;
+using DG.Tweening;
+
+public class Stickman : MonoBehaviour
+{
+
+  [ Title( "Components" ) ]
+    [ SerializeField ] Animator _animator;
+    [ SerializeField ] ColorSetter _colorSetter;
+
+	// Private 
+	RecycledSequence recycledSequence = new RecycledSequence();
+
+    void OnDisable()
+    {
+		recycledSequence.Kill();
+	}
+
+    public void AttachToVehicle( Transform parent, VehiclePartData vehiclePartData )
+    {
+		transform.parent = parent;
+
+		_animator.SetTrigger( vehiclePartData.pose );
+		var colorTween = _colorSetter.LerpColor( vehiclePartData.color, GameSettings.Instance.stickman_pose_duration );
+
+		var sequence = recycledSequence.Recycle();
+		sequence.Append( transform.DOLocalJump( vehiclePartData.position,
+			GameSettings.Instance.stickman_jump_power,
+			1,
+			GameSettings.Instance.ui_PopUp_duration ) );
+		sequence.Join( transform.DOLocalRotate( vehiclePartData.rotation, GameSettings.Instance.stickman_pose_duration ) );
+		sequence.Join( colorTween );
+	}
+
+}
