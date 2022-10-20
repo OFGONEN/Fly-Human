0762f480084a842b02067627c3f0833317994e91
ofgonen_office <ofgonen@gmail.com>
2022-10-21 00:02:19 +0300

    Stickman: DetachFromVehicle

--- Assets/Script/Stickman.cs
+++ Assets/Script/Stickman.cs
@@ -11,14 +11,22 @@ public class Stickman : MonoBehaviour
   [ Title( "Components" ) ]
     [ SerializeField ] Animator _animator;
     [ SerializeField ] ColorSetter _colorSetter;
+    [ SerializeField ] ToggleRagdoll _toggleRagdoll;
 
 	// Private 
 	RecycledSequence recycledSequence = new RecycledSequence();
+	RecycledTween    recycledTween    = new RecycledTween();
 
     void OnDisable()
     {
 		recycledSequence.Kill();
 	}
+
+    void Awake()
+    {
+		_toggleRagdoll.SwitchRagdoll( false );
+		_toggleRagdoll.ToggleCollider( true );
+    }
 
     public void AttachToVehicle( Transform parent, VehiclePartData vehiclePartData )
     {
@@ -46,4 +54,20 @@ public class Stickman : MonoBehaviour
 		sequence.Join( transform.DOLocalRotate( vehiclePartData.rotation, GameSettings.Instance.stickman_pose_duration ) );
 		sequence.Join( colorTween );
 	}
+
+    public void DetachFromVehicle()
+    {
+		transform.parent = null;
+
+		_animator.enabled = false;
+
+		_toggleRagdoll.SwitchRagdoll( true );
+		_toggleRagdoll.ToggleCollider( true );
+
+		recycledTween.Recycle( DOVirtual.DelayedCall( GameSettings.Instance.stickman_disableDuration,
+			() => gameObject.SetActive( false )
+        ) );
+	}
+
+    // public void MoveTowardsTargetVehicle( )
 }
