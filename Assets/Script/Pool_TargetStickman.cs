44a1729d60a595cb11e6d8935cc70d25a46a2d8e
ofgonen_office <ofgonen@gmail.com>
2022-10-21 16:22:01 +0300

    Implement & Create TargetStickman Pool

---
+++ Assets/Script/Pool_TargetStickman.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+
+[ CreateAssetMenu( fileName = "pool_stickman_target", menuName = "FF/Data/Pool/Target Stickman" ) ]
+public class Pool_TargetStickman : ComponentPool< TargetStickman >
+{
+}
