b11c1a7f38ddd32d136e15620571cdbb79cddc0f
ofgonen_desktop <ofgonen@gmail.com>
2022-10-22 19:00:09 +0300

    LevelData: IMPLEMENT

--- Assets/Script/FFStudio/LevelData.cs
+++ Assets/Script/FFStudio/LevelData.cs
@@ -5,15 +5,25 @@ using UnityEngine.SceneManagement;
 using Sirenix.OdinInspector;
 using System.IO;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace FFStudio
 {
 	[ CreateAssetMenu( fileName = "LevelData", menuName = "FF/Data/LevelData" ) ]
 	public class LevelData : ScriptableObject
     {
-	[ Title( "Setup" ) ]
+	[ Title( "Scene Setup" ) ]
 		[ ValueDropdown( "SceneList" ), LabelText( "Scene Index" ) ] public int scene_index;
         [ LabelText( "Override As Active Scene" ) ] public bool scene_overrideAsActiveScene;
+
+	[ Title( "Data Setup" ) ]
+		[ LabelText( "Vehicle Evolve List" ), SerializeField ] List< VehicleData > vehicle_data_array; 
+		[ LabelText( "Target Vehicle Data" ), SerializeField ] TargetVehicleData vehicle_target;
+		[ LabelText( "Last Level of Vehicle" ), SerializeField ] bool vehicle_level_last;
+		[ LabelText( "Replay Scene Indexes " ), SerializeField, ShowIf( "vehicle_level_last" ) ] int[] vehicle_level_replay;
+		[ LabelText( "Next Level of Vehicle" ), SerializeField, HideIf( "vehicle_level_last" ) ] int vehicle_level_next;
+		[ LabelText( "Start Vehicle Index" ), SerializeField ] int vehicle_start_index;
+		[ LabelText( "Start Vehicle Part Count" ), SerializeField ] int vehicle_start_count;
 
 #if UNITY_EDITOR
 		static IEnumerable SceneList()
