15acd34971a744cb1bc0d68ad66b8fbc5aac511c
ofgonen_desktop <ofgonen@gmail.com>
2022-10-22 15:20:15 +0300

    Create & Implement target stickman run time set

---
+++ Assets/Script/Set_TargetStickman.cs
@@ -0,0 +1,11 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+
+[ CreateAssetMenu( fileName = "set_target_stickman", menuName = "FF/Data/Set/Target Stickman" ) ]
+public class Set_TargetStickman : RuntimeSet< int, TargetStickman >
+{
+}
