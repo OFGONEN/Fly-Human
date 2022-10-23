ef1a4d90d0867a230be931781c778be59b4acfe2
ofgonen_office <ofgonen@gmail.com>
2022-10-24 00:03:51 +0300

    Vehicle: OnTriggerStickman

--- Assets/Script/Vehicle.cs
+++ Assets/Script/Vehicle.cs
@@ -12,13 +12,40 @@ public class Vehicle : MonoBehaviour
 // Private
     List< Stickman > vehicle_stickman = new List< Stickman >( 64 );
 	VehicleData vehicle_data;
+	int vehicle_index;
 
 	void Awake()
     {
 		var levelData = CurrentLevelData.Instance.levelData;
-		vehicle_data = levelData.vehicle_data_array[ levelData.vehicle_start_index ];
+		vehicle_index = levelData.vehicle_start_index;
+		vehicle_data  = levelData.vehicle_data_array[ vehicle_index ];
 
 		SpawnStickman( levelData.vehicle_start_count );
+	}
+
+	public void OnStickmanTrigger( StickmanGameEvent gameEvent )
+	{
+		var stickman = gameEvent.event_stickman as Stickman;
+
+		var stickman_count = vehicle_stickman.Count;
+
+		if( stickman_count + 1 > vehicle_data.VehicleCountMax )
+		{
+			if( vehicle_index < CurrentLevelData.Instance.levelData.vehicle_data_array.Length - 1 )
+				Evolve( stickman );
+			else
+				stickman.TurnIntoCurrency();
+		}
+		else
+		{
+			stickman.AttachToVehicle( transform, vehicle_data.VehiclePartAtIndex( stickman_count + 1 ) );
+			vehicle_stickman.Add( stickman );
+		}
+	}
+
+	void Evolve( Stickman incomingStickman )
+	{
+
 	}
 
     void SpawnStickman( int count )
