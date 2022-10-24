5b5b1831557e01c06ecacdcaa54d304f2f9b16e3
ofgonen_desktop <ofgonen@gmail.com>
2022-10-24 15:42:35 +0300

    VehicleData: Vehicle Collider

--- Assets/Script/VehicleData.cs
+++ Assets/Script/VehicleData.cs
@@ -9,10 +9,12 @@ public class VehicleData : ScriptableObject
     [ SerializeField ] VehiclePartData[] vehicle_data_array;
     [ SerializeField ] string vehicle_name;
     [ SerializeField, Min( 1 ) ] int vehicle_count_min;
+    [ SerializeField ] VehicleColliderData vehicle_collider;
 
-	public string VehicleName  => vehicle_name;
-	public int VehicleCountMin => vehicle_count_min;
-	public int VehicleCountMax => vehicle_data_array.Length;
+	public string VehicleName                  => vehicle_name;
+	public int VehicleCountMin                 => vehicle_count_min;
+	public int VehicleCountMax                 => vehicle_data_array.Length;
+	public VehicleColliderData VehicleCollider => vehicle_collider;
 
     public VehiclePartData VehiclePartAtIndex( int index )
     {
