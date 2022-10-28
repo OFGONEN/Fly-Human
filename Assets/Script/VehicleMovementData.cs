65e143724efe29d2031099932201646693a9a2c7
ofgonen_desktop <ofgonen@gmail.com>
2022-10-29 00:03:15 +0300

    VehicleMovementData: Landing adjust data variables

--- Assets/Script/VehicleMovementData.cs
+++ Assets/Script/VehicleMovementData.cs
@@ -9,10 +9,13 @@ using Sirenix.OdinInspector;
 public class VehicleMovementData : ScriptableObject
 {
   [ Title( "Movement Ground" ) ]
-    public float movement_ground_speed_default; 
-    public float movement_ground_speed_max; 
-    public float movement_ground_speed_accelerate_duration; 
-    public Ease movement_ground_speed_accelerate_ease; 
+    [ LabelText( "Movement Ground Speed Default" ) ] public float movement_ground_speed_default; 
+    [ LabelText( "Movement Ground Speed Max" ) ] public float movement_ground_speed_max; 
+    [ LabelText( "Movement Ground Speed Accelerate Duration" ) ] public float movement_ground_speed_accelerate_duration; 
+    [ LabelText( "Movement Ground Speed Accelerate Ease" ) ] public Ease movement_ground_speed_accelerate_ease; 
 //   [ Title( "Movement Air" ) ]
-//   [ Title( "Landing" ) ]
+  [ Title( "Landing" ) ]
+  	[ LabelText( "Landing Adjust Rotation Ease" ) ] public Ease landing_adjust_rotation_ease;
+		[ LabelText( "Landing Adjust Jump Power" ) ] public float landing_adjust_jump_power;
+		[ LabelText( "Landing Adjust Jump Ease" ) ] public Ease landing_adjust_jump_ease;
 }
