903032584ba2f25b2bc9bb03b28f9b8ae76a8833
ofgonen_desktop <ofgonen@gmail.com>
2022-10-29 19:19:55 +0300

    VehicleMovementData: Perfect eject speed

--- Assets/Script/VehicleMovementData.cs
+++ Assets/Script/VehicleMovementData.cs
@@ -13,9 +13,17 @@ public class VehicleMovementData : ScriptableObject
     [ LabelText( "Movement Ground Speed Max" ) ] public float movement_ground_speed_max; 
     [ LabelText( "Movement Ground Speed Accelerate Duration" ) ] public float movement_ground_speed_accelerate_duration; 
     [ LabelText( "Movement Ground Speed Accelerate Ease" ) ] public Ease movement_ground_speed_accelerate_ease; 
+    [ LabelText( "Movement Ground Perfect Eject Speed" ) ] public float movement_ground_speed_eject_perfect; 
 //   [ Title( "Movement Air" ) ]
   [ Title( "Landing" ) ]
   	[ LabelText( "Landing Adjust Rotation Ease" ) ] public Ease landing_adjust_rotation_ease;
-		[ LabelText( "Landing Adjust Jump Power" ) ] public float landing_adjust_jump_power;
-		[ LabelText( "Landing Adjust Jump Ease" ) ] public Ease landing_adjust_jump_ease;
+	[ LabelText( "Landing Adjust Jump Power" ) ] public float landing_adjust_jump_power;
+	[ LabelText( "Landing Adjust Jump Ease" ) ] public Ease landing_adjust_jump_ease;
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+		movement_ground_speed_eject_perfect = Mathf.Clamp( movement_ground_speed_eject_perfect, movement_ground_speed_default, movement_ground_speed_max );
+	}
+#endif
 }
