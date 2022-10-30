f454be72903d0241e8992755aae84a7109cb3173
ofgonen_office <ofgonen@gmail.com>
2022-10-30 22:28:32 +0300

    Create UI Image pool

---
+++ Assets/Script/Pool_UI_Image.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+using UnityEngine.UI;
+
+[ CreateAssetMenu( fileName = "pool_ui_image_", menuName = "FF/Data/Pool/UI Image" ) ]
+public class Pool_UI_Image : ComponentPool< Image >
+{
+}
