9311d8b69d7f5a465e832c9c69fca219b3cc74ef
ofgonen_office <ofgonen@gmail.com>
2022-11-01 12:41:53 +0300

    VehicleMovementData: Air rotate speed

--- Assets/Script/VehicleMovementData.cs
+++ Assets/Script/VehicleMovementData.cs
@@ -14,7 +14,11 @@ public class VehicleMovementData : ScriptableObject
     [ LabelText( "Movement Ground Speed Accelerate Duration" ) ] public float movement_ground_speed_accelerate_duration; 
     [ LabelText( "Movement Ground Speed Accelerate Ease" ) ] public Ease movement_ground_speed_accelerate_ease; 
     [ LabelText( "Movement Ground Perfect Eject Speed" ) ] public float movement_ground_speed_eject_perfect; 
-//   [ Title( "Movement Air" ) ]
+
+  [ Title( "Movement Air" ) ]
+  	[ LabelText( "Movement Air Rotate Speed" ) ] public float movement_air_rotate_speed;
+	[ LabelText( "Movement Air Rotate Speed Max" ) ] public float movement_air_rotate_speed_max;
+
   [ Title( "Landing" ) ]
   	[ LabelText( "Landing Adjust Rotation Ease" ) ] public Ease landing_adjust_rotation_ease;
 	[ LabelText( "Landing Adjust Jump Power" ) ] public float landing_adjust_jump_power;
